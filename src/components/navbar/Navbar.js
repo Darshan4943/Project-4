fb1ef6dafaf3470cb20dcf7c99537c60785b9aef
Darshan <darshanshinde4943@gmaul.com>
2023-05-19 20:03:34 +0530

    final done

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,23 +1,30 @@
-import React,{useState,useRef} from "react";
+import React, { useState } from "react";
 import { styled } from "@mui/material/styles";
-import { useNavigate } from 'react-router-dom';
-import {FaTrello}  from 'react-icons/fa';
-import { AiOutlineStar } from 'react-icons/ai';
-import {  FiChevronDown } from 'react-icons/fi';
-import {  AppBar,  Toolbar,  IconButton,  Typography,  TextField,  MenuItem,  Menu,Button} from "@mui/material";
-// import {  StarBorderOutlined} from "@mui/icons-material";
-import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
+import { useNavigate } from "react-router-dom";
+import { FaTrello } from "react-icons/fa";
+
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Button,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
+import ClearIcon from "@mui/icons-material/Clear";
 import styles from "./Navbar.module.css";
 
 const StyledIconButton = styled(IconButton)`
   color: white;
-  display:flex;
+  display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
 const StyledAppBar = styled(AppBar)`
-background-color: rgba(0, 0, 0, 0.616);
+  background-color: rgba(0, 0, 0, 0.616);
   backdrop-filter: blur(10px);
 `;
 
@@ -27,37 +34,20 @@ const StyledToolbar = styled(Toolbar)`
   align-items: center;
 `;
 
-const TitleInput = styled(TextField)`
-
-  ${styles.titleInput}
-  
+const ClearButton = styled(Button)`
+  color: white;
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.2);
+  }
 `;
 
 const Navbar = () => {
-  const [showWorkspaceMenu, setShowWorkspaceMenu] = useState(false);
-  const [showStarredMenu, setShowStarredMenu] = useState(false);
-  const [showRecentMenu, setShowRecentMenu] = useState(false);
-  const [isStarred, setIsStarred] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
-  const [editTitle, setEditTitle] = useState(false);
-  const [title, setTitle] =useState("Todo Management");
-  const inputRef = useRef();
-  const navigate=useNavigate();
-  
+  const [title, setTitle] = useState("Todo Management");
+  const navigate = useNavigate();
+
   const open = Boolean(anchorEl);
 
-  const toggleStarredMenu = () => {
-   setShowStarredMenu(!showStarredMenu);
-   };
-    
-   const toggleRecentMenu = () => {
-   setShowRecentMenu(!showRecentMenu);
-  };
-    
-
-  const toggleWorkspaceMenu = () => {
-  setShowWorkspaceMenu(!showWorkspaceMenu);
-  };
   const handleClose = () => {
     setAnchorEl(null);
   };
@@ -66,108 +56,56 @@ const Navbar = () => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleStarClick = () => {
-   setIsStarred(!isStarred);
-   };
-    
-
   const handleNameClick = (name) => {
-    console.log(name);
+    setTitle(name);
     handleClose();
   };
 
-  const handleTitleClick = () => {
-    setEditTitle(true);
-  };
-
-  const handleTitleBlur = () => {
-    setEditTitle(false);
-  };
-
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value);
-  };
-
-  const handleTitleKeyPress = (event) => {
-    if (event.key === "Enter") {
-      setEditTitle(false);
-      inputRef.current.blur();
-    }
+  const handleClearData = () => {
+    localStorage.clear();
   };
 
   return (
-    
-    <StyledAppBar    position="static" elevation={0}>
-      <StyledToolbar> 
+    <StyledAppBar position="static" elevation={0}>
+      <StyledToolbar>
         <StyledIconButton edge="start" color="inherit" aria-label="menu">
-        < FaTrello className={styles.logo} />   
-        <div className={styles.logoText}>Trello</div>
+          <FaTrello className={styles.logo} />
+          <div className={styles.logoText}>Trello</div>
         </StyledIconButton>
 
-        <div className={`${styles.icon} ${isStarred ? styles.starred : ''}`} onClick={handleStarClick}>
-          <AiOutlineStar />
-       </div>      
-        
-       {editTitle ? (
-  <TitleInput
-    className={`${styles.text} ${styles.editing}`}
-    inputRef={inputRef}
-    value={title}
-    onChange={handleTitleChange}
-    onBlur={handleTitleBlur}
-    onKeyPress={handleTitleKeyPress}
-    autoFocus
-    style={{ color: 'white' }} 
-  />
-) : (
-  <Typography variant="h6" className={styles.text} onClick={handleTitleClick}>
-    {title}
-  </Typography>
-)}
+        <Typography variant="h6" className={styles.text}>
+          {title}
+        </Typography>
 
+        <Button
+          onClick={() => {
+            navigate("/template");
+          }}
+          variant="contained"
+          id={styles.whiteBtn}
+          startIcon={<AddPhotoAlternateIcon />}
+        >
+          Change Background
+        </Button>
 
-        <StyledIconButton>
-        <div className={styles.menuContainer}>
-         <div className={styles.dropdown}>
-           <button className={`${styles.menuButton} ${styles.dropdownButton}`} onClick={toggleWorkspaceMenu}>
-             Workspaces <FiChevronDown className={styles.arrowIcon} />
-          </button>
-           {showWorkspaceMenu && (
-            <div className={styles.dropdownContent}>
-              <a href="#">Trello</a>
-              <a href="#">Trello</a>
-              <a href="#">Trello</a>
-            </div>
-          )}
-        </div>
-       
-        <div className={styles.dropdown}>
-          <button className={`${styles.menuButton} ${styles.dropdownButton}`} onClick={toggleStarredMenu}>
-            Starred <FiChevronDown className={styles.arrowIcon} />
-          </button>
-          {showStarredMenu && (
-            <div className={styles.dropdownContent}>
-              <a href="#">Trello</a>
-              <a href="#">Trello</a>
-              <a href="#">Trello</a>
-            </div>
-          )}
-        </div>
-        
-        <div className={styles.dropdown}>
-          <button className={`${styles.menuButton} ${styles.dropdownButton}`} onClick={toggleRecentMenu}>
-            Recent <FiChevronDown className={styles.arrowIcon} />
-          </button>
-          {showRecentMenu && (
-            <div className={styles.dropdownContent}>
-              <a href="#">Trello </a>
-              <a href="#">Trello </a>
-              <a href="#">Trello </a>
-            </div>
-          )}
-        </div>
-       </div> 
+        <StyledIconButton color="inherit" onClick={handleProfileClick}>
+          <img
+            className={styles.userImage}
+            src="https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"
+            alt="User"
+            width="50px"
+            height="50px"
+          />
         </StyledIconButton>
+
+        <ClearButton
+          onClick={handleClearData}
+          color="inherit"
+          startIcon={<ClearIcon />}
+        >
+          Clear Data
+        </ClearButton>
+
         <Menu
           id="menu-appbar"
           anchorEl={anchorEl}
@@ -182,48 +120,21 @@ const Navbar = () => {
           open={open}
           onClose={handleClose}
         >
-          <MenuItem onClick={() => handleNameClick(" Darshan Shinde")}>
+          <MenuItem onClick={() => handleNameClick("Darshan Shinde")}>
             Darshan Shinde
-          </MenuItem>
-          <MenuItem onClick={() => handleNameClick("Naga Sai Lakshmi")}>
-          Naga Sai Lakshmi
-          </MenuItem>
-          <MenuItem onClick={() => handleNameClick("Madhumita Chaudhuri")}>
-            Madhumita Chaudhuri
           </MenuItem>
           <MenuItem onClick={() => handleNameClick("Himanshu Yadav")}>
             Himanshu Yadav
           </MenuItem>
+          <MenuItem onClick={() => handleNameClick("Madhumita Chaudhuri")}>
+            Madhumita Chaudhuri
+          </MenuItem>
+          <MenuItem onClick={() => handleNameClick("Naga Sai Lakshmi")}>
+            Naga Sai Lakshmi
+          </MenuItem>
         </Menu>
-        <Button 
-               //onClick={handleImage}
-               onClick={()=>{navigate('/template')}} 
-              variant='contained' 
-              id={styles.whiteBtn} 
-              startIcon={<AddPhotoAlternateIcon />}> Change Background</Button>
-
-
-        
-
-        <StyledIconButton color="inherit" onClick={handleProfileClick}>
-          
-          <img
-       className={styles.userImage}
-
-       src="https://thumbs.dreamstime.com/b/businessman-icon-vector-male-avatar-profile-image-profile-businessman-icon-vector-male-avatar-profile-image-182095609.jpg"
-
-            alt="User"
-
-            width="50px"
-
-           height="50px" 
-            />
-       </StyledIconButton>
-       
       </StyledToolbar>
     </StyledAppBar>
-  
-   
   );
 };
 
